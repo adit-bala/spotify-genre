89300708971f5701a01824c5afbe6a0a89e6eace
adit-bala <adityarbala@gmail.com>
2023-03-27 13:24:45 -0700

    get genres from top listened to tracks

--- main.py
+++ main.py
@@ -6,11 +6,26 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 from json.decoder import JSONDecodeError
+from collections import Counter
 
 # Get username from terminal
 username = sys.argv[1]
-
 scope = 'user-top-read'
+
+
+# abstract to get genres from artists and tracks at the same time
+def get_top_genres(time_range='long_term'):
+    top_genres = Counter()
+    results = sp.current_user_top_tracks(time_range=time_range, limit=50)
+    for item in results['items']:
+        artist_id = item['artists'][0]['id']
+        artist = sp.artist(artist_id)
+        top_genres.update(artist['genres']) 
+    print(time_range.upper())
+    for i, item in enumerate(top_genres.most_common(10)):
+        print(i+1, item[0])
+    print()
+
 
 # Erase cache and prompt for user permission
 try:
@@ -22,10 +37,9 @@ except (AttributeError, JSONDecodeError):
 # Create our spotify object with permissions
 sp = spotipy.Spotify(auth=token)
 ranges = ['short_term', 'medium_term', 'long_term']
+for rng in ranges:
+    get_top_genres(rng)
 
-for sp_range in ranges:
-    print("range:", sp_range)
-    results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
-    for i, item in enumerate(results['items']):
-        print(i, item['name'], '//', item['artists'][0]['name'])
-    print()
+
+
+
